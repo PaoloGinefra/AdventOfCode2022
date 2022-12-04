28a3a71db3eae12b548ffd7ca9d7bf52186e3626
PaoloGinefra <ginefra.paolo@gmail.com>
2022-12-04 21:29:35 +0100

    Day1-1

---
+++ Day1/1-1.cpp
@@ -0,0 +1,37 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    string num;
+    ifstream file;
+
+    int count = 0;
+    int max = 0;
+
+    file.open("inp1.txt");
+
+    while (getline(file, num))
+    {
+        if (!num.empty())
+        {
+            count += stoi(num);
+
+            if (count > max)
+                max = count;
+        }
+        else
+            count = 0;
+    }
+
+    if (count > max)
+        max = count;
+
+    cout << max << endl;
+
+    file.close();
+}
